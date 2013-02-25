8cf7e64efc6aa147f5dc646446746d2ce778f97d
andrrr <andrey.lubinov@gmail.com>
2013-02-25 17:05:25 +0200

    tuning configuration

--- assets/richtext_tinymce.publish.js
+++ assets/richtext_tinymce.publish.js
@@ -11,23 +11,28 @@ jQuery(document).ready(function() {
 		script_url: Symphony.Context.get('root') + '/extensions/richtext_tinymce/lib/tiny_mce.js',
 
 		theme: 'advanced',
-		plugins: '',
+		plugins: 'paste,fullscreen,table,advimage',
 		skin: 'symphony',
 		width: '100%',
-		theme_advanced_buttons1 : 'formatselect,bold,italic,underline,|,bullist,numlist,outdent,indent,|,blockquote,link,unlink,image,|,copy,paste,pastetext,pasteword,|,undo,redo,|,cleanup,code',
-		theme_advanced_buttons2 : '',
+		language: 'ru',
+		theme_advanced_buttons1 : 'fullscreen,|,pastetext,pasteword,|,undo,redo,|,formatselect,bold,italic,underline,|,bullist,numlist,outdent,indent,hr,|,blockquote,link,unlink,image',
+		theme_advanced_buttons2 : 'forecolor,|,justifyleft,justifyright,justifycenter,justifyfull,|,tablecontrols,|,cleanup,code',
 		theme_advanced_buttons3 : '',
 		theme_advanced_buttons4 : '',
 		theme_advanced_toolbar_location : 'top',
 		theme_advanced_toolbar_align : 'left',
 		theme_advanced_resizing : true,
+		extended_valid_elements : 'img[!src|border:0|alt|title|width|height|style]a[name|href|target|title|onclick]',
 
 		template_external_list_url : 'lists/template_list.js',
 		external_link_list_url : 'lists/link_list.js',
 		external_image_list_url : 'lists/image_list.js',
 		media_external_list_url : 'lists/media_list.js',
-		
-		entities: ''
+		paste_auto_cleanup_on_paste : true,
+
+		// entities: '',
+		// encoding : 'xml',
+		entity_encoding : 'raw'
 		
 	});
 	
